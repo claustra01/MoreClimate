cb3ddd40332311fd45823a74eefe27f8dbcbdaaf
sny430 <shinya430.g@gmail.com>
2022-11-24 03:37:14 +0900

    :recycle: refactor WeightedVinePlantHandler.java

--- src/main/java/claustra01/moreclimate/compat/immersiveengineering/crop/WisteriaPlantHandler.java
+++ src/main/java/claustra01/moreclimate/compat/immersiveengineering/crop/WisteriaPlantHandler.java
@@ -2,15 +2,21 @@ package claustra01.moreclimate.compat.immersiveengineering.crop;
 
 import blusunrize.immersiveengineering.api.ComparableItemStack;
 import claustra01.moreclimate.compat.immersiveengineering.WeightedItemStack;
-import claustra01.moreclimate.compat.immersiveengineering.WeightedVinePlantHandler;
+import claustra01.moreclimate.compat.immersiveengineering.WeightedPlantHandler;
+import defeatedcrow.hac.api.blockstate.DCState;
 import defeatedcrow.hac.food.FoodInit;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class WisteriaPlantHandler extends WeightedVinePlantHandler {
+public class WisteriaPlantHandler extends WeightedPlantHandler {
 
     public static float seedWeight = 0.25f;
     public static float cropWeight = 1f;
@@ -34,6 +40,20 @@ public class WisteriaPlantHandler extends WeightedVinePlantHandler {
                 new WeightedItemStack(itemDrops, cropWeight)
         };
         return WeightedItemStack.getRandomDrops(rand, stacks);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IBlockState[] getRenderedPlant(ItemStack seed, ItemStack soil, float growth, TileEntity tile) {
+        List<IBlockState> states = new ArrayList<IBlockState>();
+        states.add(crop.getDefaultState());
+
+        IBlockState[] ret = new IBlockState[states.size()];
+        for (int i = 0; i < states.size(); i++)
+            if (states.get(i) != null)
+                ret[i] = (states.get(i).getBlock()).getDefaultState().withProperty(DCState.STAGE4, Math.min(3, Math.round(3 * growth)));
+        return ret;
+
     }
 
 }
